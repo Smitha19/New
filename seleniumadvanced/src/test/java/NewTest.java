2514e0c90f8ed2ee6345f2cf966a3fe92cce5053
smitha.prabhath <smitha.prabhath@BDC3-LX-6441MYW.dir.svc.accenture.com>
2018-09-18 12:16:38 +0530

    Commit

---
+++ seleniumadvanced/src/test/java/NewTest.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void testDragAndDrop() {
+	  WebDriver driver=new ChromeDriver();
+	  driver.get
+  }
+}
